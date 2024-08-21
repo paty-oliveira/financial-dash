f812fe2df560725e97bfb8bd99ebdb18e589d955
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-08-21 19:03:48 +0100

    Experimenting streamlit features

--- src/app.py
+++ src/app.py
@@ -1,8 +1,74 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
+with st.container():
+    st.write("Here's our first attempt at using data to create a table:")
+    st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
-}))
+    }))
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.table(dataframe)
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+var = st.session_state.name
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
+
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+st.sidebar.write("-------")
+st.sidebar.header("Introduction")
+
+
