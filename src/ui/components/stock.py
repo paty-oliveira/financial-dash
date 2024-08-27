54a76160aeb8d5e22b045974b7235b1e903fd7bf
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-08-27 20:12:00 +0100

    Rename Stock Tabs

--- src/ui/components/stock.py
+++ src/ui/components/stock.py
@@ -1,9 +1,9 @@
 import streamlit_shadcn_ui as ui
 
-from .balance_sheet_tab import render as balance_sheet
-from .cashflow_tab import render as cashflow
-from .income_stmt_tab import render as income_stmt
-from .overview_tab import render as overview
+from .balance_sheet import render as balance_sheet
+from .cashflow import render as cashflow
+from .income_stmt import render as income_stmt
+from .overview import render as overview
 
 tab_content = {
     "Overview": overview,
