6edf587dc665f14181b29b41d2a3b6c8f78ba2ca
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-08-27 20:10:22 +0100

    Add stock component

--- src/ui/components/container.py
+++ src/ui/components/container.py
@@ -1,28 +1,13 @@
-import streamlit as st
-import streamlit_shadcn_ui as ui
-
-from .balance_sheet_tab import render as balance_sheet
-from .cashflow_tab import render as cashflow
-from .income_stmt_tab import render as income_stmt
-from .overview_tab import render as overview
+from .homepage import render as homepage
+from .stock import render as stock
 from ..state import Ticker
-
-tab_content = {
-    "Overview": overview,
-    "Balance Sheet": balance_sheet,
-    "Income Statement": income_stmt,
-    "CashFlow": cashflow,
-}
 
 
 def render():
     ticker_name = Ticker()
 
     if ticker_name.is_available():
-        options = list(tab_content.keys())
-        current_tab = ui.tabs(options, default_value=options[0], key="current_tab")
-        return tab_content[current_tab]()
+        return stock()
 
     else:
-        st.write("welcome page")
-        st.write("Write a valid ticker to start your analysis!")
+        return homepage()
---
+++ src/ui/components/homepage.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+def render():
+    st.write("welcome page")
+    st.write("Write a valid ticker to start your analysis!")
