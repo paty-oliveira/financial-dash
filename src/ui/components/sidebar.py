93511cc00fd6c500cf64a0525369b9f1da359c5e
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-08-28 14:00:01 +0100

    Change text input label in sidebar

--- src/ui/components/sidebar.py
+++ src/ui/components/sidebar.py
@@ -4,4 +4,4 @@ import streamlit as st
 def render():
     with st.sidebar:
         st.title("Dashboard :bar_chart:")
-        st.text_input("Write a valid Ticker", key="ticker", type="default")
+        st.text_input("Enter a stock ticker:", key="ticker", type="default")
