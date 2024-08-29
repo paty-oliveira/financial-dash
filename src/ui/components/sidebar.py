a93892d6088efc092a53c4bbce0a953a819b8426
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-08-29 19:53:39 +0100

    Add donation button

--- src/ui/components/sidebar.py
+++ src/ui/components/sidebar.py
@@ -1,7 +1,9 @@
 import streamlit as st
+from streamlit_extras.buy_me_a_coffee import button
 
 
 def render():
     with st.sidebar:
         st.title("Dashboard :bar_chart:")
         st.text_input("Enter a stock ticker:", key="ticker", type="default")
+        button(username="devpatrici0", floating=True)
