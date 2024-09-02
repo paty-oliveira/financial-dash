f6e54ec5bf906a79e5bf760cdef2ed60032cafb2
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-09-02 19:01:44 +0100

    Add Line Chart in Overview tab

--- src/ui/components/overview.py
+++ src/ui/components/overview.py
@@ -3,4 +3,17 @@ import streamlit as st
 
 def render(_data_provider):
     data = _data_provider.get_stock_info()
-    st.json(data)
+    historical_data = _data_provider.get_historical_data(period="max", interval="1d")[
+        "Close"
+    ]
+    col1, col2 = st.columns(2, gap="large")
+    container = st.container()
+
+    with col1:
+        st.line_chart(historical_data, x_label="Date", y_label="Price")
+
+    with col2:
+        st.write("Placeholder for Company Summary")
+
+    with container:
+        st.write(f"{data['longBusinessSummary']}")
