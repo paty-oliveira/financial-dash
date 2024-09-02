e416cf269e0cad0d79220bccdb993fca60af4ade
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-09-02 18:47:17 +0100

    Change footer rendering

--- src/ui/components/container.py
+++ src/ui/components/container.py
@@ -1,3 +1,4 @@
+from .footer import render as footer
 from .homepage import render as homepage
 from .stock import render as stock
 from ..data_providers.FinancialDataProviderFactory import FinancialDataProviderFactory
@@ -11,6 +12,7 @@ def render():
         ticker_name = ticker.get_ticker()
         data_provider = FinancialDataProviderFactory(ticker_name).get_provider()
         stock(data_provider)
+        footer()
 
     else:
         homepage()
--- src/ui/components/stock.py
+++ src/ui/components/stock.py
@@ -1,9 +1,7 @@
 from .content import render as content
-from .footer import render as footer
 from .header import render as header
 
 
 def render(_data_provider):
     header(_data_provider)
     content(_data_provider)
-    footer()
