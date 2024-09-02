46ad741175dfee6d9e0d300ab63b2c8ad68c2212
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-09-02 18:46:08 +0100

    Rename Tabs to Content

--- src/ui/components/stock.py
+++ src/ui/components/stock.py
@@ -1,9 +1,9 @@
+from .content import render as content
 from .footer import render as footer
 from .header import render as header
-from .tabs import render as tabs
 
 
 def render(_data_provider):
     header(_data_provider)
-    tabs(_data_provider)
+    content(_data_provider)
     footer()
