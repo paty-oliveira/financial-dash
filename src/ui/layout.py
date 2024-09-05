96b8de3186f529e4badd734f1f9878d9f4dca399
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-09-05 15:58:52 +0100

    Add Header

--- src/ui/layout.py
+++ src/ui/layout.py
@@ -27,13 +27,8 @@ def render_footer():
 
 
 def render_stock_info(financial_data):
-    stock_info = financial_data.get_stock_info(
-        st.session_state["ticker"],
-    )
     # Render header
-    st.markdown(
-        f"### {stock_info['symbol']} - {stock_info['longName']}", unsafe_allow_html=True
-    )
+    render_header(financial_data)
     # Renders tabs
     tab_titles = ["Overview", "Balance Sheet", "Income Statement", "CashFlow"]
     tabs_renderings = [
@@ -48,6 +43,31 @@ def render_stock_info(financial_data):
             render_action(financial_data)
 
 
+def render_header(financial_data):
+    stock_info = financial_data.get_stock_info(
+        st.session_state["ticker"],
+    )
+    st.markdown(
+        f"### {stock_info['symbol']} - {stock_info['longName']}", unsafe_allow_html=True
+    )
+    # Render price change
+    current_price = stock_info["currentPrice"]
+    previous_close_price = stock_info["previousClose"]
+    currency = stock_info["currency"]
+    current_price_content = f"{convert_currency_symbol(currency)}{current_price}"
+    price_diff = calculate_price_changes(current_price, previous_close_price)
+    price_diff_content = f"({price_diff}%)"
+    price_change_content = (
+        apply_text_color(price_diff_content, "red")
+        if price_diff < 0
+        else apply_text_color(price_diff_content, "green")
+    )
+    st.markdown(
+        f"#####  {current_price_content} {price_change_content}",
+        unsafe_allow_html=True,
+    )
+
+
 def render_overview(financial_data):
     ticker = st.session_state["ticker"]
     historical_data = financial_data.get_historical_data(
@@ -57,12 +77,14 @@ def render_overview(financial_data):
         interval="1d",
     )
     quotes = financial_data.get_stock_info(ticker)
-    col1, col2 = st.columns(2, gap="large")
+    stock_performance, ratios_summary = st.columns(2, gap="large")
 
-    with col1:
+    with stock_performance:
+        st.markdown("#### Stock Performance")
         st.line_chart(historical_data, x_label="Date", y_label="Price")
 
-    with col2:
+    with ratios_summary:
+        st.markdown("#### Summary")
         st.write("Placeholder for Company Summary")
 
     with st.container():
@@ -82,3 +104,38 @@ def render_income_stmt(financial_data):
 def render_cashflow(financial_data):
     cashflow = financial_data.get_cashflow(st.session_state["ticker"])
     st.dataframe(cashflow)
+
+
+def convert_currency_symbol(currency_symbol):
+    currency = {
+        "USD": "$",  # US Dollar
+        "EUR": "€",  # Euro
+        "JPY": "¥",  # Japanese Yen
+        "GBP": "£",  # British Pound Sterling
+        "AUD": "A$",  # Australian Dollar
+        "CAD": "C$",  # Canadian Dollar
+        "CHF": "CHF",  # Swiss Franc
+        "CNY": "¥",  # Chinese Yuan Renminbi
+        "HKD": "HK$",  # Hong Kong Dollar
+        "INR": "₹",  # Indian Rupee
+        "RUB": "₽",  # Russian Ruble
+        "BRL": "R$",  # Brazilian Real
+        "ZAR": "R",  # South African Rand
+        "KRW": "₩",  # South Korean Won
+        "MXN": "$",  # Mexican Peso
+        "SGD": "S$",  # Singapore Dollar
+        "NZD": "NZ$",  # New Zealand Dollar
+        "TRY": "₺",  # Turkish Lira
+        "SEK": "kr",  # Swedish Krona
+        "NOK": "kr",  # Norwegian Krone
+    }
+
+    return currency[currency_symbol]
+
+
+def calculate_price_changes(current_price, previous_close_price):
+    return round((current_price - previous_close_price) / previous_close_price, 2) * 100
+
+
+def apply_text_color(text, color):
+    return f"<span style='color:{color}'>{text}</span>"
