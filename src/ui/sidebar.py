ca3d1bc68dd9c4e4025387c4793ae03215afbdef
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-09-26 20:05:08 +0100

    Add user's feedback component in sidebar

--- src/ui/sidebar.py
+++ src/ui/sidebar.py
@@ -7,3 +7,14 @@ def render():
         st.title("Dashboard :bar_chart:")
         st.text_input("Enter a stock ticker:", key="ticker", type="default")
         button(username="devpatrici0", floating=True)
+
+        with st.container():
+            st.write("### We would love your feedback!")
+            st.text_input("Name:", key="username", type="default")
+            st.text_input("Email:", key="user_email", type="default")
+            # user_feedback = st.text_area(
+            #     "Please provide your suggestions or feedback here:",
+            #     key="feedback",
+            # )
+            # app_rating = st.feedback("stars")
+            st.button("Submit", type="primary", use_container_width=True)
