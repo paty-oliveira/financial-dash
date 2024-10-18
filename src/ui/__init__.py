75645f0198e9e0124eac8ceedc954957df7862c9
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-10-18 18:09:54 +0100

    Remove Google Analytics Tracking

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,8 +1,6 @@
 import os
-import pathlib
 
 import streamlit as st
-from bs4 import BeautifulSoup
 
 from .layout import render as render_layout
 from .sidebar import render as render_sidebar
@@ -14,7 +12,6 @@ initial_state = {"ticker": "", "balance_sheet_frequency": "yearly"}
 def run(financial_data, financial_calculations):
     feedback_form_url = os.getenv("FEEDBACK_FORM_URL")
     donation_url = os.getenv("DONATION_URL")
-    ga_tracking_id = os.getenv("GA_TRACKING_ID")
 
     st.set_page_config(
         page_title="Stock Analysis Dashboard",
@@ -22,31 +19,6 @@ def run(financial_data, financial_calculations):
         layout="wide",
         initial_sidebar_state="expanded",
     )
-
-    # Add Google Analytics Tracking
-    google_analytics_content = f"""
-        <script async src="https://www.googletagmanager.com/gtag/js?id={ga_tracking_id}"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-
-            function gtag() {{
-                dataLayer.push(arguments);
-            }}
-
-            gtag('js', new Date());
-
-            gtag('config', {ga_tracking_id});
-        </script>
-    """
-    # Insert the script in the head tag of the static template inside your virtual environment
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    soup = BeautifulSoup(index_path.read_text(), "lxml")
-
-    if not soup.find(id="ga-script"):
-        script_tag = soup.new_tag("script", id="ga-script")
-        script_tag.string = google_analytics_content
-        soup.head.append(script_tag)
-        index_path.write_text(str(soup))
 
     # Injecting global CSS stylesheet
     st.markdown(global_stylesheet, unsafe_allow_html=True)
